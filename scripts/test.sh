6639ce3e4a8f27d0620c942f783983cdbf23e44f
Lian Jiang <lian.jiang@opendoor.com>
2023-11-06 09:30:54 -0800

    bug fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 set -x -e
 
-pip install pipx
-pipx install gh:jiangok2006/cookiecutter-remix
+pip install cookiecutter
+cookiecutter gh:jiangok2006/cookiecutter-remix
 cd cookiecutter-remix
 npm install
 npm run dev &
