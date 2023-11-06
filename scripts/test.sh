b5278a96db1946ebeff12ce28b25b3419840dd98
Lian Jiang <lian.jiang@opendoor.com>
2023-11-06 15:44:40 -0800

    dbg

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 set -x -e
 
 pip install cookiecutter
-cookiecutter --no-input gh:jiangok2006/cookiecutter-remix
+cookiecutter --no-input gh:jiangok2006/cookiecutter-remix/jiangok2006-patch-1
 cd cookiecutter_remix
 echo "listing files..."
 ls -al
