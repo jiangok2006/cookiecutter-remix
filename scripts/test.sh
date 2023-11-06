d7668a5128e46505b1ca16c1be3a52bd20a498ae
Lian Jiang <lian.jiang@opendoor.com>
2023-11-06 15:22:28 -0800

    debug

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,8 @@ set -x -e
 pip install cookiecutter
 cookiecutter --no-input gh:jiangok2006/cookiecutter-remix
 cd cookiecutter_remix
+echo "listing files..."
+ls -al
 npm install
 npm run dev &
 # npx playwright install --with-deps
