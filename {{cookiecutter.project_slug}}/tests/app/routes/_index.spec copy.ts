82a30e6e595612fae8109dbed14764a702b2d91d
Lian Jiang <lian.jiang@opendoor.com>
2023-11-07 02:10:59 -0800

    cleanup

--- {{cookiecutter.project_slug}}/tests/app/routes/_index.spec copy.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('The page should show remix contacts', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await expect(page.getByRole('heading', { name: 'Welcome to Remix' })).toHaveCount(1);
-});
