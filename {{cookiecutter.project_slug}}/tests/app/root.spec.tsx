a34d002fdac770616d21747912e23fcd2ebbbdbe
Lian Jiang <lian.jiang@opendoor.com>
2023-11-06 16:53:10 -0800

    dbg

--- {{cookiecutter.project_slug}}/tests/app/root.spec.tsx
+++ {{cookiecutter.project_slug}}/tests/app/root.spec.tsx
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('The page should show remix contacts', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  expect(page.getByRole('heading', { name: 'Remix Contactss' })).toBeVisible;
+  await expect(page.getByRole('heading', { name: 'Remix Contactss' })).toHaveCount(1);
 });
