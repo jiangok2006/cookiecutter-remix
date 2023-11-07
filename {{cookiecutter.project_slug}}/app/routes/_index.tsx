7b5ec5c0915a2ba907f36e9d3705a0148bd625d4
jiangok2006 <jiangok2006@gmail.com>
2023-11-07 05:37:08 -0800

    enable tailwind (#12)

--- {{cookiecutter.project_slug}}/app/routes/_index.tsx
+++ {{cookiecutter.project_slug}}/app/routes/_index.tsx
@@ -1,16 +1,21 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { name: "description", content: "Welcome to Remix" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
       <ul>
         <li>
           <a
