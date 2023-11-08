f6cde9c218324c5e179340d29f9cb04a90a95939
Lian Jiang <lian.jiang@opendoor.com>
2023-11-07 20:02:59 -0800

    dbg

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,12 +8,15 @@ cd cookiecutter_remix
 npm install
 npx playwright install --with-deps
 
-if [ "$1" == "unit" ] then
+if [ "$1" == "unit" ]; then
+    echo "Running unit tests"
     npm run test:unit
-elif [ "$1" == "integration" ] then
+elif [ "$1" == "integration" ]; then
+    echo "Running integration tests"
     npm run test:integration
 else
+    echo "Running e2e tests"
     npm run dev &
     npm run test:e2e
-endif
+fi
 
