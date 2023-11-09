166bdffaf55405d45fd6c73187b3f94894208166
Lian Jiang <lian.jiang@opendoor.com>
2023-11-09 10:05:20 -0800

    dbg

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,7 +16,7 @@ export DATABASE_URL="postgres://postgres:postgres@${POSTGRES_HOST}:${POSTGRES_PO
 function start_db {
     docker-compose up -d
     echo '🟡 - Waiting for database to be ready...'
-    $DIR/cookiecutter_remix/scripts/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
+    $DIR/scripts/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
     npx prisma migrate dev --name init
 }
 
