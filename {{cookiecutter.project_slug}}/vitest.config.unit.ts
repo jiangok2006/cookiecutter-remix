aba87475ec3436045baefe22208fc7e23b8ebcb7
jiangok2006 <jiangok2006@gmail.com>
2023-11-09 12:07:26 -0800

    code coverage (#21)

--- {{cookiecutter.project_slug}}/vitest.config.unit.ts
+++ {{cookiecutter.project_slug}}/vitest.config.unit.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['tests/unit/**/*.test.ts'],
+    coverage: {
+      reporter: ['text', 'json-summary', 'json'],
+    }
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, ".") }]
