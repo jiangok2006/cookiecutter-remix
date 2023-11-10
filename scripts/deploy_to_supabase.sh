0b59d70ffe38b4ce51093cc0558047f9c2fad57c
Lian Jiang <lian.jiang@opendoor.com>
2023-11-10 07:48:09 -0800

    db

--- scripts/deploy_to_supabase.sh
+++ scripts/deploy_to_supabase.sh
@@ -5,5 +5,8 @@ set -ex
 DIR=$(pwd) 
 
 echo "deploy to supabase $1 ..."
-
+echo "ls 2..."
+ls $DIR
+echo "ls 3..."
+ls $DIR/cookiecutter-remix/prisma
 npx prisma migrate deploy --schema=$DIR/cookiecutter-remix/prisma/schema.prisma
