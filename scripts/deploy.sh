cf00c561ce322984de7460d528148a06b891d91e
Lian Jiang <lian.jiang@opendoor.com>
2023-11-10 08:16:46 -0800

    d

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,13 @@ set -ex
 
 DIR=$(pwd) 
 
+
+echo "deploy to cloudflare $1 ..."
+
 pip install cookiecutter
 cookiecutter --no-input .
 cd cookiecutter_remix
 npx pnpm install
 
 npx prisma migrate deploy
+npx pnpm run pages:deploy --project-name cookiecutter-remix
