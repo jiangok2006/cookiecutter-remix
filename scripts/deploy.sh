7707411b4391b2299b94987442178fcacdc4dd20
Lian Jiang <jiangok2006@gmail.com>
2023-11-13 11:20:10 -0800

    d

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,8 +12,5 @@ cookiecutter --no-input .
 cd cookiecutter_remix
 npx pnpm install
 
-echo "deploy to supabase ..."
-npx prisma migrate deploy
-
 echo "deploy to cloudflare ..."
-npx pnpm run pages:deploy --project-name cookiecutter-remix
+npx pnpm run pages:deploy:staging --project-name cookiecutter-remix
