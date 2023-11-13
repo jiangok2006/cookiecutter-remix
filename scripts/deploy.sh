7275502cb9b7b33242af01013b53a7a1a2b16ccb
Lian Jiang <jiangok2006@gmail.com>
2023-11-13 11:20:37 -0800

    d

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,4 +13,4 @@ cd cookiecutter_remix
 npx pnpm install
 
 echo "deploy to cloudflare ..."
-npx pnpm run pages:deploy:staging --project-name cookiecutter-remix
+npx pnpm run pages:deploy:$1 --project-name cookiecutter-remix
