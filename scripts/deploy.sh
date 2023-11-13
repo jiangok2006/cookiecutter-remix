671b63ca4d0557da944df2422e721bdaea751f1b
Lian Jiang <jiangok2006@gmail.com>
2023-11-13 12:42:59 -0800

    d

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,10 @@ set -ex
 
 DIR=$(pwd) 
 
-
-echo "deploy to $1 ..."
-
 pip install cookiecutter
 cookiecutter --no-input .
 cd cookiecutter_remix
 npx pnpm install
 
 echo "deploy to cloudflare ..."
-npx pnpm run pages:deploy:$1 --project-name cookiecutter-remix
+npx pnpm run pages:deploy --project-name cookiecutter-remix
