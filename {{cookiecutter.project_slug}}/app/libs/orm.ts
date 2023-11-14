ca58eb4f08df85f96fa6c34ff3cacb8b2f1e7b03
jiangok2006 <jiangok2006@gmail.com>
2023-11-14 04:18:43 -0800

    unit test using in memory sqlite (#36)

--- {{cookiecutter.project_slug}}/app/libs/orm.ts
+++ {{cookiecutter.project_slug}}/app/libs/orm.ts
@@ -1,5 +1,9 @@
+import { customers } from "../../schema/customers";
 
 export interface Env {
     DB: D1Database;
 }
 
+
+export type Customer = typeof customers.$inferSelect; // return type when queried
+export type NewCustomer = typeof customers.$inferInsert; // insert type
