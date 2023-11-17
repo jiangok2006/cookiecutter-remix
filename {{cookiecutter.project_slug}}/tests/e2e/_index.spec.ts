0196ee614476092ab54efea4a55eb7ed29b3d7f3
jiangok2006 <jiangok2006@gmail.com>
2023-11-16 17:38:00 -0800

    enable e2e test (#38)

--- {{cookiecutter.project_slug}}/tests/e2e/_index.spec.ts
+++ {{cookiecutter.project_slug}}/tests/e2e/_index.spec.ts
@@ -1,23 +1,25 @@
-// import { Browser, Page, chromium, expect } from "@playwright/test";
-// import { afterAll, beforeAll, describe, test } from "vitest";
-// import { httpUrl } from '../common/setup';
+import { Browser, Page, chromium, expect } from "@playwright/test";
+import { afterAll, beforeAll, describe, test } from "vitest";
+import { httpUrl } from '../common/setup';
 
 
-// describe("playwright meets vitest", () => {
-//   let page: Page;
-//   let browser: Browser;
-//   beforeAll(async () => {
-//     browser = await chromium.launch();
-//     let context = await browser.newContext();
-//     page = await context.newPage();
-//   });
+describe("playwright meets vitest", () => {
+    let page: Page;
+    let browser: Browser;
+    beforeAll(async () => {
+        browser = await chromium.launch();
+        let context = await browser.newContext();
+        page = await context.newPage();
+    });
 
-//   afterAll(async () => {
-//     await browser.close();
-//   });
+    afterAll(async () => {
+        if (browser) {
+            await browser.close();
+        }
+    });
 
-//   test('The page should show remix string', async () => {
-//     await page.goto(httpUrl!);
-//     await expect(page.getByText('Welcome to Remix', { exact: true })).toBeDefined()
-//   });
-// })
+    test('The page should show remix string', async () => {
+        await page.goto(httpUrl!);
+        await expect(page.getByText('Welcome to Remix', { exact: true })).toBeDefined()
+    });
+})
