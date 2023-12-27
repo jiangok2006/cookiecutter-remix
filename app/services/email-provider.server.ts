d0cb0be9bcee3e3059a3db5a0a7002acc3112d13
Lian Jiang <jiangok2006@gmail.com>
2023-12-27 02:54:18 -0800

    d

--- app/services/email-provider.server.ts
+++ app/services/email-provider.server.ts
@@ -7,7 +7,8 @@ export const sendEmail = async (
     subject: string, body: string
 ) => {
     console.log(`email-provider, sendEmail: ${JSON.stringify(options)}, ${subject}, ${body}`);
-    // https://blog.cloudflare.com/sending-email-from-workers-with-mailchannels
+    // https://blog.cloudflare.com/sending-email-from-workers-with-mailchannels this sends an email of text.
+    // https://support.mailchannels.com/hc/en-us/articles/4565898358413-Sending-Email-from-Cloudflare-Workers-using-MailChannels-Send-API this sends an email of html.
     const response = await fetch('https://api.mailchannels.net/tx/v1/send', {
         method: 'POST',
         headers: {
@@ -20,14 +21,14 @@ export const sendEmail = async (
                 },
             ],
             from: {
-                email: 'support@mycoolapp.site',
+                email: 'no-reply@mycoolapp.site',
                 name: 'mycoolapp',
             },
             subject: subject,
             content: [
                 {
-                    type: 'text/plain',
-                    value: body,
+                    type: 'text/html',
+                    value: "<html><body><p>Hi there!</p><a href=\"https://www.bing.com\">Click here</a></body></html>",
                 },
             ],
         }),
