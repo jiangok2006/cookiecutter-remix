3448dcd33f248a902ed964bfacf3c6146d654928
Lian Jiang <jiangok2006@gmail.com>
2023-12-29 03:57:34 -0800

    d

--- migrations/0000_uneven_annihilus.sql
+++ migrations/0000_uneven_annihilus.sql
@@ -1,12 +1,4 @@
-CREATE TABLE `sessions` (
-	`uuid` text PRIMARY KEY NOT NULL,
-	`user_id` integer NOT NULL,
-	`data` text,
-	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`updated_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`expires_at` integer NOT NULL
-);
---> statement-breakpoint
+
 CREATE TABLE `users` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`email` text,
