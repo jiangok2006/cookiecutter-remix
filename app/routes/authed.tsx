99a8625a94f77bf246c615b3e1326ec9483ba896
jiangok2006 <jiangok2006@gmail.com>
2023-12-31 15:59:22 -0800

    enable logout (#57)

--- app/routes/authed.tsx
+++ app/routes/authed.tsx
@@ -1,8 +1,9 @@
 
-import type { LoaderFunction, LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { type LoaderFunction, type LoaderFunctionArgs } from "@remix-run/cloudflare";
 import {
     Link,
-    Outlet
+    Outlet,
+    useLoaderData
 } from "@remix-run/react";
 import type { Env } from "../libs/orm";
 import { auth } from "../services/auth.server";
@@ -11,26 +12,35 @@ import { auth } from "../services/auth.server";
 export let loader: LoaderFunction = async ({ request, context }: LoaderFunctionArgs) => {
     let env = context.env as Env;
     console.log('env.disable_auth', env.disable_auth)
+    let user: { id: number; email: string | null; created_at: Date; updated_at: Date; } | null = null;
     if (env.disable_auth === 'false') {
-        await auth(
+        user = await auth(
             env.DB,
             env.magic_link_secret,
             env.cookie_secret)
             .isAuthenticated(request, { failureRedirect: '/login' })
     }
 
-    return null
+    return { user };
 };
 
 
 export default function Index() {
+    let { user } = useLoaderData<typeof loader>()
+
     return (
         <>
-            <div className="flex flex-row bg-stone-300">
-                <span className="p-6">MyCoolApp</span>
-                <Link to="/authed/cj" className="p-6 bg-orange-300 hover:bg-orange-100">CJ</Link>
-                <Link to="/authed/ebay" className="p-6 bg-orange-300 hover:bg-orange-100">eBay</Link>
-                <Link to="/authed/google" className="p-6 bg-orange-300 hover:bg-orange-100">Google</Link>
+            <div className="h-16 flow-root bg-slate-100">
+                <div className="flex flex-row h-full float-left  ">
+                    <div className="p-5">MyCoolApp</div>
+                    <Link to="/authed/cj" className="p-5  bg-orange-300 hover:bg-orange-100 ">CJ</Link>
+                    <Link to="/authed/ebay" className="p-5  bg-orange-300 hover:bg-orange-100 ">eBay</Link>
+                    <Link to="/authed/google" className="p-5 bg-orange-300 hover:bg-orange-100">Google</Link>
+                </div>
+                <div className="flex flex-row-reverse h-full float-right">
+                    <Link to="/logout" className=" p-5 bg-orange-300 hover:bg-orange-100 ">Logout</Link>
+                    <div className="p-5">{user?.email ?? "localtest"}</div>
+                </div>
             </div>
             <div className="p-5">
                 <Outlet />
