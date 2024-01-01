c6522d783aab21ff658ed31fb311d55985474aac
jiangok2006 <jiangok2006@gmail.com>
2023-12-31 18:59:34 -0800

    ebay notification (#62)

--- app/routes/ebay_notification.ts
+++ app/routes/ebay_notification.ts
@@ -4,15 +4,17 @@ import type { Env } from "../libs/orm";
 
 
 // https://developer.ebay.com/marketplace-account-deletion
-export let loader: LoaderFunction = async ({ params, context }: LoaderFunctionArgs) => {
+export let loader: LoaderFunction = async ({ request, context }: LoaderFunctionArgs) => {
+
     let env = context.env as Env;
-    let { challengeCode } = params;
+    const url = new URL(request.url);
+    const challengeCode = url.searchParams.get("challenge_code");
+    console.log(`get challengeCode: ${challengeCode}`);
     const hash = createHash('sha256');
     hash.update(challengeCode!);
     hash.update(env.ebay_verification_token);
     hash.update(env.ebay_notification_endpoint);
     const responseHash = hash.digest('hex');
-    console.log(Buffer.from(responseHash).toString());
 
     return new Response(responseHash, {
         headers: {
