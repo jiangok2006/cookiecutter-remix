4b467c9e7b33545cce3e1870811f687693fa7558
Lian Jiang <jiangok2006@gmail.com>
2023-12-31 17:40:38 -0800

    d

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -4,9 +4,6 @@ import { users, type User } from "../schema/user";
 
 export const getUserByEmail = async (db: D1Database, email: string): Promise<User> => {
     let result = await drizzle(db).select().from(users).where(eq(users.email, email)).execute();
-    if (result.length === 0) {
-        result = await drizzle(db).select().from(users).where(eq(users.email, email)).execute();
-    }
     console.log(`getUserByEmail result: ${JSON.stringify(result)}`);
     return result[0] as User
 }
