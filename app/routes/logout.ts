490cb906a6a01f4af8ba3474e3bf2821f6479b25
jiangok2006 <jiangok2006@gmail.com>
2023-12-31 17:26:16 -0800

    logout works

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -4,11 +4,14 @@ import type { Env } from "../libs/orm";
 import { createCookieSessionStorageWithVars } from "../services/session.server";
 
 export let loader: LoaderFunction = async ({ request, context }: LoaderFunctionArgs) => {
-    console.log('logout is called')
-
     let env = context.env as Env;
     let { getSession, destroySession } = createCookieSessionStorageWithVars(env.cookie_secret)
     let session = await getSession(request.headers.get('Cookie'))
-    await destroySession(session)
-    return redirect('/login');
+    return redirect('/login',
+        {
+            headers: {
+                "Set-Cookie": await destroySession(session),
+            }
+        }
+    );
 }
