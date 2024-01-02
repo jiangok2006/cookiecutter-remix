ab2a0fcf42f99d13bacc757e731909937f4401f4
Lian Jiang <jiangok2006@gmail.com>
2024-01-02 06:47:51 -0800

    ebay_consent accept/reject pages.

--- app/routes/ebay_consent_rejected.ts
+++ app/routes/ebay_consent_rejected.ts
@@ -1,8 +1,8 @@
 import type { LoaderFunction, LoaderFunctionArgs } from "@remix-run/cloudflare";
 
 export let loader: LoaderFunction = async ({ request, context }: LoaderFunctionArgs) => {
-    console.log(`consent is received. request.url: ${request.url}`);
-    return `consent is received, ${await request.text()}`
+    console.log(`consent is rejected. request.url: ${request.url}`);
+    return `consent is rejected, ${await request.text()}`
 };
 
 
