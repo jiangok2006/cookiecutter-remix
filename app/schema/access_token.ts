eeaab0910734c45690773a318d4fbdfb423f3436
jiangok2006 <jiangok2006@gmail.com>
2024-01-14 16:07:22 -0800

    Dev (#82)

--- app/schema/access_token.ts
+++ app/schema/access_token.ts
@@ -9,11 +9,11 @@ export const access_tokens = sqliteTable('access_tokens', {
     id: integer('id').primaryKey(),
     provider: text('provider').notNull().unique(), // cj, ebay, google
     access_token: text('access_token'),
-    access_token_expires_at: integer('access_token_expires_at', { mode: 'timestamp_ms' }),
+    access_token_expires_at: integer('access_token_expires_at', { mode: 'timestamp' }),
     refresh_token: text('refresh_token'), // google has no this field for subsequent requests after the first.
-    refresh_token_expires_at: integer('refresh_token_expires_at', { mode: 'timestamp_ms' }), // google has no this field
-    created_at: integer('created_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
-    updated_at: integer('updated_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+    refresh_token_expires_at: integer('refresh_token_expires_at', { mode: 'timestamp' }), // google has no this field
+    created_at: integer('created_at', { mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+    updated_at: integer('updated_at', { mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
 });
 
 export type AccessToken = typeof access_tokens.$inferSelect
