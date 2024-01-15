19b43d3fc7cf78644ac609e51e55afa6630e515f
jiangok2006 <jiangok2006@gmail.com>
2024-01-14 23:02:39 -0800

    Dev (#95)

--- app/routes/authed.google._index.tsx
+++ app/routes/authed.google._index.tsx
@@ -58,14 +58,25 @@ export let loader: LoaderFunction = async ({ request, context }: LoaderFunctionA
     if (!gTokenPairsMap.get(gProvider)) {
         console.log(`google redirecting to consent api`)
         // https://developers.google.com/identity/protocols/oauth2/web-server#sample-oauth-2.0-server-response
+
+        // Step 1: Set authorization parameters
+        // https://developers.google.com/identity/protocols/oauth2/web-server#creatingclient
+
+        // Why I don't need to do STEP 1?
+        // Why I cannot get refresh_token at all?
+
         let params = new URLSearchParams();
         params.append('client_id', env.google_client_id!);
         params.append('redirect_uri', env.google_redirect_uri!);
         params.append('response_type', 'code');
         params.append('scope', env.google_scopes);
         params.append('state', env.google_consent_api_state);
-        params.append('access_type', "offline");
+        // Again, refresh_token is only present in this response if you set the access_type parameter 
+        // to offline in the initial request to Google's authorization server. 
+        params.append('access_type', 'offline');
 
+        // Step 2: Redirect to Google's OAuth 2.0 server
+        // https://developers.google.com/identity/protocols/oauth2/web-server#redirecting
         let consentUrl = `${env.google_auth_host}?${params.toString()}`
         return redirect(consentUrl)
     }
